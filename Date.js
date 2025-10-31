570136a2da41418a6842518d56d191c25eca0f1e
ajayreddy1202 <ajayreddyvishnu@gmail.com>
2025-10-31 17:50:34 +0530

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,48 @@
+const date =new Date()
+
+// 1.getFullYear():-
+console.log("current year:",date.getFullYear())
+
+
+// 2.getMonth():-returns number from 0 to 11 (0 - january)
+console.log("display the month:",date.getMonth())
+
+// 3.getDay():-returns number from 0 to 6 (0 - sunday)
+console.log("display current day:",date.getDay())
+
+// 4.getDate():-returns today's date
+console.log("display the date :",date.getDate())
+
+// 5.getHours():- returns current hours in 24 format
+console.log("display the hours:",date.getHours())
+
+// 6.getMinutes():-
+console.log("display the minutes:",date.getMinutes())
+
+// 7.getSeconds():-
+console.log("display seconds:",date.getSeconds())
+
+//8. getMilliseconds():-
+console.log("display the milliseconds:",date.getMilliseconds())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
