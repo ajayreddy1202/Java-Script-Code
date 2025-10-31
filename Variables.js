56fd23451576195cc56bbee30142607c66f7eb46
ajayreddy1202 <ajayreddyvishnu@gmail.com>
2025-10-31 18:14:48 +0530

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,80 @@
+// //1.Using var
+// var studentName="Ajay"
+// console.log("Student who use mobile phone in class:",studentName)
+// //document.writeln("Student who use mobile phone in class:",studentName)
+
+// //scope
+// {
+//     console.log("Student name inside the block:",studentName)
+//     var institue="Pyspiders"
+//     console.log("Institute name inside the block:",institue)
+// }
+// console.log("Institue name outside the block:",institue)
+// console.log("Another block:",institue)
+
+
+// //Re-intialisation
+// studentName="Anil"
+// console.log("Student name after re-init:",studentName)
+
+// //Re-declaration
+// var studentName="Kishor"
+// console.log("Student name after re-declaration:",studentName)
+// //console.log(----------------------------------------)
+// //var shouldn't be used in production,because bugs can be introduced into program at any point.
+
+
+
+
+// //2.using let
+//  let collegeName = "audisankara";
+// console.log("college name:", collegeName);
+
+// {
+//     console.log("college name inside the block:", collegeName);
+//     let place = "nellore";
+//     console.log("college place:", place);
+// }
+
+// //Re-intialisation
+// collegeName = "sank";
+// console.log("student name re_init:", collegeName);
+
+// // Re-declaration 
+// let place = "gudur";
+// console.log("college place re_declaration:", place);
+
+
+
+// 3.const
+// const gravity=9.8
+// console.log("Gravity:",gravity)
+
+// {
+//     const adharNum=934794640912
+// }
+// cosle.log("Adhar card number:",adharNum)
+
+// re-int - not possible
+// gravity=10
+// console.log("Gravity after re-init:",gravity) TypeError:Assignment to constant variable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
