24efbaa1e373d9bd9d72e25647d6f1e048dab4e6
ajayreddy1202 <ajayreddyvishnu@gmail.com>
2025-10-31 18:10:12 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,41 @@
+//3.COMPARISION OPERATORS
+let a =100
+let b ='100'
+let c =101
+let d ='101'
+
+console.log("a==b:",a==b)
+console.log("a===b:",a===b)
+
+
+//Ternary Operator
+result = a == b ? "a&b are equal":"a&b are not equal"
+console.log("Result:",result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
