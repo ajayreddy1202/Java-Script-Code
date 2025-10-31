23cc6b5d989f31cab2fe6f774b676156664e2786
ajayreddy1202 <ajayreddyvishnu@gmail.com>
2025-10-31 18:13:00 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,186 @@
+// let finisher="Ajay reddy"
+// for(let i=0; i<finisher.length;i++){
+//     console.log(finisher[i])
+// }
+
+
+// // Reverse string without using built-in method
+// let reversed=''
+// for(let i=finisher.length-1; i>=0;i--){
+//     reversed +=finisher[i]
+// }
+// console.log("Reversed string:",reversed)
+
+
+// // BUILT-IN METHODS:
+// const prompt = require("prompt-sync")({signt:true});
+// // 1.toUpperCase():
+// let s=" ajay reddy "
+// console.log(s.toUpperCase());
+
+// // 2.toLowerCase():
+// console.log(s.toLowerCase())
+
+// // 3.length():
+// console.log(s.length)
+
+// // 4.concat():
+// let batch='M77'
+// let res1=finisher.concat(batch)
+// console.log("Combine string:",res1)
+
+
+// // 5.replace():
+// // console.log(s.replace(r))
+// let s1 = "I like apples";
+// let r = s1.replace("apples", "mangoes");
+// console.log(r);   // Output: I like mangoes
+
+
+// // 6.slice():
+// console.log(s.slice(2))
+
+// //7.split():
+// let insti="pyspiders btm bengolor"
+// let splitted1=insti.split(" ")
+// console.log(splitted1)
+// let splitted2=insti.split("a")
+// console.log(splitted2)
+// let splitted3=insti.split("")
+// console.log(splitted3)
+
+// // reverse the string 
+// let a=s.split('')
+// console.log(a.reverse())
+
+
+// // 8.trim(): remove the white space
+// console.log(s.trim())
+
+// // 9.trimstart():
+// console.log(s.trimStart())
+
+// // 10.trimend():
+// console.log(s.trimEnd())
+
+
+// // 11.padStart():
+// console.log(s.padStart(15,"*"))
+
+// // 12.padEnd():
+// console.log(s.padEnd(15,"*"))
+
+
+
+// // let Num = prompt("Enter the number:")
+// // console.time()
+// // console.log(Num.slice(0,4).padEnd(20,"*").concat(Num.slice(16,20).padStart(20,"*")))
+// // console.timeEnd()
+
+
+// // 13.indexOf():
+// let City="Bangolore"
+// console.log("index of first 'a':",City.indexOf("a"))
+// console.log("index of first 'a':",City.indexOf("a",2))
+
+
+// // 14.lastindexOf():
+// let insti2="Pyspiders BTM Layout Bengolore"
+
+
+// // 15.charAt(): -returns the character which is present at specified index,
+// // if character is not Present, returns undifined.
+// let str = "JavaScript";
+
+// console.log(str.charAt(0));   // Output: J
+// console.log(str.charAt(4));   // Output: S
+// console.log(str.charAt(10));  // Output: ""  (empty string, since index 10 doesn’t exist)
+
+
+
+// // 16. charCodeAt(): -Returns the ASCII value of the characte which is present at specified.
+// console.log(insti2.charCodeAt(6))
+
+
+// // a=>ASCII: charCodeAt()
+// // ASCII=> a: String.fromCharCode()
+
+// console.log(String.fromCharCode)
+// let chr= '';
+// for(let i = 65; i <=122; i++) if((i <= 90 ) || (i >= 97))
+//     chr += String.fromCharCode(i)
+// console.log(chr)
+
+
+// 19.includes():- checks wether given string is included in original string or not. returns boolean.
+// console.log(insti2.includes("Layout"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
